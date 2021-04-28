56dcb6e4fd9b1f8168122c327ec6becf9891dc45
miralps <miralspatel96@gmail.com>
2021-04-27 17:01:12 -0700

    jquery file

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+$(".study1").hover("click", function(){
+    $(this).css("background-color","red");
+   
+
+// $(".study1").on("hover",function(){
+// $(this).addClass("transform");
+// $(this).effect("bounce", {times:1},2000);
+    
+// });
+
+// $('.study1 button').on("click", function(){
+//     $('.cssChange').css("background-color","red");
+//   });
+
+
+
+
+
+
+});
+});
