b976730b2a711f190ef02723f637520c0740693d
miralps <miralspatel96@gmail.com>
2021-05-01 15:51:36 -0700

    added about.html and new images

--- js/index.js
+++ js/index.js
@@ -1,23 +1,35 @@
+
 $(document).ready(function(){
-    $(".study1").hover(function(){
-      $(this).width(1.25*$(this).width());
-      $(this).height(1.25*$(this).height());
+  $('.workGrid>*').hover(function(){
+    $(this).css({"width":"85vw","height":"42vh"});
+  },function(){
+    $(this).css({"width":"80vw","height":"40vh"});
+  });
+  });
+
+  
+
+  // old jquery
+
+  // $(document).ready(function(){
+//     $(".study1").hover(function(){
+//       $(this).width(1.25*$(this).width());
+//       $(this).height(1.25*$(this).height());
     
-    },function(){
-        $(this).width(0.8*$(this).width());
-        $(this).height(0.8*$(this).height());
+//     },function(){
+//         $(this).width(0.8*$(this).width());
+//         $(this).height(0.8*$(this).height());
 
-    });
+//     });
 
 
-    // $('.study1').hover(function(){
-    //     $(this).css("transform", "scale(1.1, 1.1)");
-    //  }, function(){
-    //     $(this).css("transform", "none");
-    //  });
+//     // $('.study1').hover(function(){
+//     //     $(this).css("transform", "scale(1.1, 1.1)");
+//     //  }, function(){
+//     //     $(this).css("transform", "none");
+//     //  });
 
 
   
 
-});
-
+// });
